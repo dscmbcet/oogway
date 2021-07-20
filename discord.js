e03483724c456abad0998ef9634ae56bdd4c3274
M Aswin Kishore <mak26.work@gmail.com>
2021-07-20 22:20:45 +0530

    fix: command error

--- discord.js
+++ discord.js
@@ -30,5 +30,6 @@ client.on('message', (message) => {
 	const args = message.content.slice(prefix.length).trim().split(' ');
 	const command = args.shift().toLowerCase();
 
-	client.commands.get(command).execute(message, args);
+	const commandFile = client.commands.get(command);
+	commandFile !== undefined ? commandFile.execute(message, args) : null;
 });
