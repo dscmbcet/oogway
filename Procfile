010a5b0f6c87efe2c90988cf75f60ce73a655972
M Aswin Kishore <mak26.work@gmail.com>
2021-07-21 00:41:29 +0530

    Testing worker dyno

---
+++ Procfile
@@ -0,0 +1,1 @@
+Worker: nodemon discord.js
--- discord.js
+++ discord.js
@@ -1,36 +1,39 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-const { client, prefix } = require('./app');
+const Discord = require("discord.js");
+const fs = require("fs");
+require("dotenv").config();
+const { prefix, token } = JSON.parse(process.env.CONFIG);
+const client = new Discord.Client();
+client.login(token);
 
 client.commands = new Discord.Collection();
 const commandFiles = fs
-	.readdirSync('./commands/')
-	.filter((file) => file.endsWith('.js'));
+  .readdirSync("./commands/")
+  .filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
-client.on('ready', () => console.log('Bot Is Ready!'));
+client.on("ready", () => console.log("Bot Is Ready!"));
 
 // Listening to a new user
-client.on('guildMemberAdd', (member) => {
-	// Send the message to a designated channel on a server:
-	const channel = member.guild.channels.cache.find(
-		(ch) => ch.name === 'member-log',
-	);
-	// Do nothing if the channel wasn't found on this server
-	if (!channel) return;
-	// Send the message, mentioning the member
-	channel.send(`Welcome to the DSC server, ${member}`);
+client.on("guildMemberAdd", (member) => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(
+    (ch) => ch.name === "member-log"
+  );
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the DSC server, ${member}`);
 });
 
 // listening to messages
-client.on('message', (message) => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-	const args = message.content.slice(prefix.length).trim().split(' ');
-	const command = args.shift().toLowerCase();
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(" ");
+  const command = args.shift().toLowerCase();
 
-	const commandFile = client.commands.get(command);
-	commandFile !== undefined ? commandFile.execute(message, args) : null;
+  const commandFile = client.commands.get(command);
+  commandFile !== undefined ? commandFile.execute(message, args) : null;
 });
