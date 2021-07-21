4876509ed776b31cfc6aefab6b4d55d5b64e72b6
M Aswin Kishore <mak26.work@gmail.com>
2021-07-21 18:04:47 +0530

    Added le-debut channel create and delete commands

---
+++ commands/team-create-channel.js
@@ -0,0 +1,120 @@
+const Discord = require("discord.js");
+const colors = require("../utils/colors");
+
+module.exports = {
+    name: "team-create-channel",
+    usage: "!team-create-channel <TEAM_NO> <CHANNEL_NAME>",
+    description: "Creates Channel <CHANNEL_NAME> with given no. of <TEAM_NO> as sub channels having void and text and creates role for each team",
+
+    /**
+     * @param {Discord.Message} message The Message
+     * @param {string[]} args The arguments
+     */
+    async execute(message, args) {
+        if (args.length < 2)
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        else {
+            if (!message.member.hasPermission('ADMINISTRATOR')) {
+                embed = new Discord.MessageEmbed({
+                    description: `You are not wise enough to make those channels my friend** ${member}**`,
+                    color: colors.red,
+                })
+                return message.channel.send({ embed });
+            }
+
+            const TEAM_NO = args[0];
+            args.splice(0, 1);
+            const CHANNEL_NAME = args.join(" ");
+
+            const CateogoryRole = await message.guild.roles.create({
+                data: {
+                    name: CHANNEL_NAME,
+                    color: 'BLACK',
+                },
+            })
+
+            const catergory = await message.guild.channels.create(CHANNEL_NAME, {
+                type: 'category',
+                permissionOverwrites: [
+                    {
+                        id: CateogoryRole.id,
+                        allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'CONNECT']
+                    },
+                    {
+                        id: message.guild.roles.everyone,
+                        deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'CONNECT']
+                    }
+                ]
+            })
+
+            for (let i = 1; i <= TEAM_NO; i++) {
+                const role = await message.guild.roles.create({
+                    data: {
+                        name: `team ${i}`,
+                        color: 'RANDOM',
+                    },
+                })
+
+                await message.guild.channels.create(`team ${i}`, {
+                    type: 'text',
+                    parent: catergory.id,
+                    permissionOverwrites: [
+                        {
+                            id: role.id,
+                            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'CONNECT']
+                        },
+                        {
+                            id: message.guild.roles.everyone,
+                            deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'CONNECT']
+                        }
+                    ]
+                });
+
+                await message.guild.channels.create(`team ${i}`, {
+                    type: 'voice',
+                    parent: catergory.id,
+                    permissionOverwrites: [
+                        {
+                            id: role.id,
+                            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'CONNECT']
+                        },
+                        {
+                            id: message.guild.roles.everyone,
+                            deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'CONNECT']
+                        }
+                    ]
+                });
+            }
+
+            const annoucementChannel = await message.guild.channels.create(`announcements`, {
+                type: 'text',
+                position: 1,
+                parent: catergory.id,
+                permissionOverwrites: [
+                    {
+                        id: CateogoryRole.id,
+                        allow: ['VIEW_CHANNEL'],
+                        deny: ['SEND_MESSAGES']
+                    },
+                    {
+                        id: message.guild.roles.everyone,
+                        deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'CONNECT']
+                    }
+                ]
+            });
+
+            let embed = new Discord.MessageEmbed({
+                title: "Created Channel Succesfully",
+                color: colors.green,
+            });
+
+            let reaction_embed = new Discord.MessageEmbed({
+                title: "Reaction Embed : TODO",
+                color: colors.cyan,
+            });
+
+            await message.channel.send({ embed });
+            await annoucementChannel.send(reaction_embed);
+        }
+    },
+};
---
+++ commands/team-delete-channel.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const colors = require("../utils/colors");
+
+module.exports = {
+    name: "team-delete-channel",
+    usage: "!team-delete-channel <CHANNEL_NAME>",
+    description: "Deletes Channel <CHANNEL_NAME> with all its sub channels and roles",
+
+    /**
+     * @param {Discord.Message} message The Message
+     * @param {string[]} args The arguments
+     */
+    async execute(message, args) {
+        if (!args.length)
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        else {
+            if (!message.member.hasPermission('ADMINISTRATOR')) {
+                embed = new Discord.MessageEmbed({
+                    description: `You are not wise enough to delete those channels my friend** ${member}**`,
+                    color: colors.red,
+                })
+                return message.channel.send({ embed });
+            }
+
+            const CHANNEL_NAME = args.join(" ");
+            const category = await message.guild.channels.cache.find(cat => cat.name === CHANNEL_NAME);
+            category.children.forEach(async channel => {
+                const team_role = await message.guild.roles.cache.find(rol => rol.name === channel.name);
+                if (team_role) await team_role.delete();
+                await channel.delete()
+            });
+
+            const role = await message.guild.roles.cache.find(rol => rol.name === CHANNEL_NAME);
+            if (role) await role.delete();
+            await category.delete();
+
+            let embed = new Discord.MessageEmbed({
+                description: `Deleted Channel: '${CHANNEL_NAME}' Succesfully`,
+                color: colors.red,
+            });
+            message.channel.send({ embed });
+        }
+    },
+};
