28ee40d04daa3be26e2f68c6b463c112318f9ca0
M Aswin Kishore <mak26.work@gmail.com>
2021-07-24 20:08:30 +0530

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -18,13 +18,17 @@ module.exports = {
     if (args[0] > 100) return message.channel.send(embed.setDescription(`You cannot delete more than 100 messages my friend ${message.author}!`));
     if (args[0] < 1) return message.channel.send(embed.setDescription(`You must delete at least one message my friend ${message.author}!`));
 
-    const messages = await message.channel.messages.fetch({ limit: args[0] })
-    message.channel.bulkDelete(messages);
-
-    embed = new Discord.MessageEmbed()
-      .setDescription(`${messages.size} Messages Deleted By ${message.author}`)
-      .setColor(colors.green);
-
+    const messages = await message.channel.messages.fetch({ limit: args[0] });
+    try {
+      await message.channel.bulkDelete(messages);
+      embed = new Discord.MessageEmbed()
+        .setDescription(`${messages.size} Messages Deleted By ${message.author}`)
+        .setColor(colors.green);
+    } catch (e) {
+      embed = new Discord.MessageEmbed()
+        .setDescription(`Messages older than 2 weeks cannot be deleted`)
+        .setColor(colors.red);
+    }
     return message.channel.send(embed);
   }
 }
