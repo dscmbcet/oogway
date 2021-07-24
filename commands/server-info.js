6d332b97ffa36a03d636fa2cf8783ec8aff09ff6
M Aswin Kishore <mak26.work@gmail.com>
2021-07-24 17:49:43 +0530

    Update server-info.js

--- commands/server-info.js
+++ commands/server-info.js
@@ -12,19 +12,24 @@ module.exports = {
    * @param {string[]} args
    */
   async execute(message, args) {
-    const { name, memberCount, region, channels, roles } = message.guild;
-    const channelData = channels.cache;
+    const { name, memberCount, region, channels, roles, owner, id, members, premiumSubscriptionCount } = message.guild;
     let embed = new Discord.MessageEmbed()
       .setTitle("Server Information")
       .setColor(colors.orange)
       .setThumbnail(message.guild.iconURL())
       .addField("Server Name", name, true)
-      .addField("Total members", memberCount, true)
+      .addField("Server ID", id, true)
+      .addField("Server Owner", owner, true)
       .addField("Server Region", region, true)
-      .addField("Categories", channelData.filter(channel => channel.type == "category").size, true)
-      .addField("Text Channels", channelData.filter(channel => channel.type == "text").size, true)
-      .addField("Voice Channels", channelData.filter(channel => channel.type == "voice").size, true)
-      .addField("Roles", roles.cache.map(roles => `${roles}`).sort().join(' '));
+      .addField("Boost Count", premiumSubscriptionCount || '0', true)
+      .addField("Roles", roles.cache.size, true)
+      .addField("Total members", memberCount, true)
+      .addField("Humans", members.cache.filter(member => !member.user.bot).size, true)
+      .addField("Bots", members.cache.filter(member => member.user.bot).size, true)
+      .addField("Categories", channels.cache.filter(channel => channel.type == "category").size, true)
+      .addField("Text Channels", channels.cache.filter(channel => channel.type == "text").size, true)
+      .addField("Voice Channels", channels.cache.filter(channel => channel.type == "voice").size, true)
+
     return message.channel.send(embed);
   },
 };
