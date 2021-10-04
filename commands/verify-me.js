5d69d7122363df76fba9a2e9e62359cf0303a84b
M Aswin Kishore <mak26.work@gmail.com>
2021-10-04 23:11:15 +0530

    fix: prevent reverification

--- commands/verify-me.js
+++ commands/verify-me.js
@@ -1,6 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 const Discord = require('discord.js');
-const { PREFIX } = require('../utils/constants');
+const { PREFIX, COLORS } = require('../utils/constants');
 const { sendVerify } = require('../events/guildMemberAdd');
+const { getMember } = require('../firebase/firebase_handler');
 
 module.exports = {
     name: 'verify-me',
@@ -12,6 +14,13 @@ module.exports = {
      */
     async execute(message) {
         const member = message.guild.member(message.author.id);
+        const embed = new Discord.MessageEmbed()
+            .setTitle('Verification')
+            .setColor(COLORS.red)
+            .setDescription(`You are already verified ${member.user}`);
+
+        const tempUser = await getMember(member);
+        if (tempUser.verified) return message.channel.send(embed);
         await sendVerify(member, message.channel);
     },
 };
