efd410374c992ad06a4936e1d6907e19d1646ddb
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2022-01-19 14:43:32 +0530

    feat: log member_ban and member_kick events

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,13 +1,20 @@
 const Discord = require('discord.js'); // eslint-disable-line no-unused-vars
 const { updateBanOrKickMember } = require('../firebase/ban');
+const { COLORS } = require('../utils/constants');
+const { logger } = require('../utils/logger');
 
 module.exports = {
     name: 'guildMemberRemove',
 
     /**
      * @param {Discord.GuildMember} member
+     * @param {Discord.Client} client
      * */
-    async execute(member) {
+    async execute(member, client) {
+        const serverConfig = client.configs.get(member.guild.id);
+        const channel = client.channels.cache.get(serverConfig.moderator_channel_id);
+        const embed = new Discord.MessageEmbed();
+
         const bannedLog = (
             await member.guild.fetchAuditLogs({
                 limit: 1,
@@ -25,11 +32,25 @@ module.exports = {
         if (bannedLog && bannedLog.createdAt > member.joinedAt) {
             if (bannedLog.target.id === member.id) {
                 updateBanOrKickMember(member, bannedLog, undefined, true);
+                embed
+                    .setTitle(`${member.user.tag} has been banned`)
+                    .setThumbnail(member.user.displayAvatarURL())
+                    .setDescription(`Reason: ${!bannedLog.reason ? '-' : bannedLog.reason}\n\nBy: \`${bannedLog.executor.tag}\``)
+                    .setColor(COLORS.cyan);
+                logger.info(`${member.user.tag} has been banned`);
+                channel.send(embed);
             }
         }
         if (kickLog && kickLog.createdAt > member.joinedAt) {
             if (kickLog.target.id === member.id) {
                 updateBanOrKickMember(member, undefined, kickLog, true);
+                embed
+                    .setTitle(`${member.user.tag} has been kicked`)
+                    .setThumbnail(member.user.displayAvatarURL())
+                    .setDescription(`Reason: ${!kickLog.reason ? '-' : kickLog.reason}\n\nBy: \`${kickLog.executor.tag}\``)
+                    .setColor(COLORS.cyan);
+                logger.info(`${member.user.tag} has been kicked`);
+                channel.send(embed);
             }
         }
     },
