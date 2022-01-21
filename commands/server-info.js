cb2bf2f9ccebc5673eb95be8a6ed5f1a4ca1b685
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2022-01-21 22:15:43 +0530

    fix: remove deprecated info and add unverified count

--- commands/server-info.js
+++ commands/server-info.js
@@ -9,9 +9,12 @@ module.exports = {
     /**
      * @param {Discord.Message} message
      * @param {string[]} args
+     * @param {Discord.Client} client
      */
-    async execute(message) {
-        const { name, memberCount, region, channels, roles, owner, id, members, premiumSubscriptionCount } = message.guild;
+    async execute(message, args, client) {
+        const serverConfig = client.configs.get(message.guild.id);
+        const { name, memberCount, channels, roles, owner, id, members, premiumSubscriptionCount } = message.guild;
+
         const embed = new Discord.MessageEmbed()
             .setTitle('Server Information')
             .setColor(COLORS.orange)
@@ -19,11 +22,11 @@ module.exports = {
             .addField('Server Name', name, true)
             .addField('Server ID', id, true)
             .addField('Server Owner', owner, true)
-            .addField('Server Region', region, true)
             .addField('Boost Count', premiumSubscriptionCount || '0', true)
             .addField('Roles', roles.cache.size, true)
             .addField('Total members', memberCount, true)
             .addField('Humans', members.cache.filter((member) => !member.user.bot).size, true)
+            .addField('Unverified', members.cache.filter((member) => member.roles.cache.has(serverConfig.unverified_role_id)).size, true)
             .addField('Bots', members.cache.filter((member) => member.user.bot).size, true)
             .addField('Categories', channels.cache.filter((channel) => channel.type === 'category').size, true)
             .addField('Text Channels', channels.cache.filter((channel) => channel.type === 'text').size, true)
