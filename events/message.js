f812997081b99f3e61ba01818974bd294bbfb305
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2022-10-23 15:35:22 +0530

    feat: prevent @everyone usage by non-core team

--- events/message.js
+++ events/message.js
@@ -57,21 +57,23 @@ module.exports = {
     },
 
     /**
-     * Checks if given message is a nitro spam message
+     * Checks if given message is a spam message
      * @param {Discord.Message} message
      * @param {Discord.Client} client
      * */
     async checkSpamMessage(message, client) {
-        const msg = message.content;
+        const msg = message.content.toLowerCase();
 
         const spamArray = firebaseSpamLinkArray.map((e) => e.link);
 
         let flag = false;
-        if (msg.toLowerCase().includes('@everyone') && msg.toLowerCase().includes('nitro')) {
+        const spamKeys = ['nitro'];
+
+        if (msg.includes('@everyone') && spamKeys.some((e) => msg.includes(e))) {
             flag = true;
         } else {
             for (let index = 0; index < spamArray.length; index++) {
-                if (msg.toLowerCase().includes(spamArray[index])) {
+                if (msg.includes(spamArray[index])) {
                     flag = true;
                     break;
                 }
@@ -86,7 +88,8 @@ module.exports = {
                         `Dear ${member},`,
                         'We have noticed you are not following our guidelines properly',
                         'As mentioned earlier we will not tolerate any kind of misbehaviour,',
-                        'You are being `KICKED` from **GsDSC MBCET Server**',
+                        'You are being `KICKED` from **GDSC MBCET Server**',
+                        `\nYour Message :-\n\`\`\`${message.content}\`\`\``,
                     ].join('\n')
                 );
             } catch (error) {}
@@ -96,6 +99,43 @@ module.exports = {
 
             const embed = new Discord.MessageEmbed()
                 .setTitle(`${member.user.tag} used spam link`)
+                .setDescription(`Message :-\n\`\`\`${message.content}\`\`\``)
+                .setThumbnail(member.user.displayAvatarURL())
+                .setColor(COLORS.cyan);
+
+            message.delete();
+
+            const serverConfig = client.configs.get(message.guild.id);
+            const channel = client.channels.cache.get(serverConfig.moderator_channel_id);
+            channel.send(embed);
+        } else {
+            this.checkNonPrivilegeEveryoneUse(message, client);
+        }
+    },
+
+    /**
+     * Checks if given message has @everyone and it's not a core team
+     * @param {Discord.Message} message
+     * @param {Discord.Client} client
+     * */
+    async checkNonPrivilegeEveryoneUse(message, client) {
+        const msg = message.content.toLowerCase();
+
+        if (msg.includes('@everyone') && !message.member.permissions.has('MANAGE_CHANNELS')) {
+            const member = message.guild.member(message.author.id);
+            try {
+                await member.send(
+                    [
+                        `Dear ${member},`,
+                        'Please avoid the usage of @everyone.',
+                        'As it will send alert to every member in the server.',
+                        `\nYour Message :-\n\`\`\`${message.content}\`\`\``,
+                    ].join('\n')
+                );
+            } catch (error) {}
+
+            const embed = new Discord.MessageEmbed()
+                .setTitle(`${member.user.tag} used @everyone`)
                 .setDescription(`Message :-\n\`\`\`${message.content}\`\`\``)
                 .setThumbnail(member.user.displayAvatarURL())
                 .setColor(COLORS.cyan);
